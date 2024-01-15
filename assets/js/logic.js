d5af830ecf2dd1c6b690a50e0780d046db60a489
rober-web <robp.medeiros@gmail.com>
2024-01-15 00:46:31 +0000

    added runningGame function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,9 @@
 
+import theQuestions from '../json/theQuestions.json' assert { type: 'json' };
+
+
 let start = document.getElementById('start');
+let startScreen = document.getElementById('start-screen');
 let questions = document.getElementById('questions');
 let questionTitle = document.getElementById('question-title');
 let choices = document.getElementById('choices');
@@ -10,6 +14,10 @@ let submit = document.getElementById('submit');
 let feedback = document.getElementById('feedback');
 let timeContent = document.getElementById('time');
 let time;
+let score = 0;
+let wins = 0;
+let loses = 0;
+
 
 // ## Acceptance Criteria
 
@@ -19,12 +27,23 @@ let time;
 
 
 // A start button that when clicked a timer starts and the first question appears.
-start.addEventListener('click', startQuiz);
+
 
 const startQuiz = () => {
-    time = 30; // Set initial time (in seconds)
-    displayTime(); // Display the initial time
 
+    //turn the start screen off
+    startScreen.children[1].style.display = 'none';
+
+   
+    //disable start button to avoid running the timer twice
+    start.setAttribute('disabled', 'disabled'); 
+    questions.classList.remove('hide');
+    runningGame(); //start the game
+
+    time = 30; // Set initial time (in seconds)
+
+    displayTime(); // Display the initial time
+    
     // Start the countdown timer
     let timerInterval = setInterval(() => {
         time--;
@@ -35,8 +54,13 @@ const startQuiz = () => {
         if (time <= 0) {
             // end the quiz
             clearInterval(timerInterval); // Stop the timer
+
+            //enable the start button back after timer stops
+            start.removeAttribute('disabled');
         }
     }, 1000);
+    
+    
 }
 
 const displayTime = () => {
@@ -45,6 +69,7 @@ const displayTime = () => {
     timeContent.textContent = time;
 }
  
+start.addEventListener('click', startQuiz);
 //   * Questions contain buttons for each answer.
 //   * 
 //   * When answer is clicked, the next question appears
@@ -54,3 +79,38 @@ const displayTime = () => {
 // * The quiz should end when all questions are answered or the timer reaches 0.
 
 //   * When the game ends, it should display their score and give the user the ability to save their initials and their score
+
+/*  const questionsJson = ()=>{
+
+    fetch('./assets/json/questions.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+}  */
+
+const runningGame = () => {
+
+    // Check if theQuestions and questions array are defined
+    if (theQuestions && theQuestions.questions && theQuestions.questions.length > 0) {
+        // Display the title of the first question
+        questionTitle.append(theQuestions.questions[0].question);
+       // choices.append(theQuestions.questions[0].options[0]);
+
+      for(let i=0; i<theQuestions.questions[0].options.length; i++){
+            let theChoiceButton = document.createElement('button');
+            let questionList = theQuestions.questions[0].options[i];
+
+            theChoiceButton.innerHTML = questionList;
+            choices.append(theChoiceButton);
+        } 
+
+
+
+        console.log(questionTitle.textContent = theQuestions.questions[0].question);
+    } else {
+        console.error('Questions array is undefined or empty.');
+    }
+}
+
+
+//runningGame();
